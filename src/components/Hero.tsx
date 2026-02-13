61e4e69c1d539d04d30eca33ffd2c63e331fc7a0
wanjugucode <karobiaanastasia>
2026-02-13 10:53:17 +0300

    updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import heroImage from "@/assets/mk5.png";
+import heroImage from "@/assets/mk5.jpg";
 
 const Hero = () => {
   const scrollToContact = () => {
